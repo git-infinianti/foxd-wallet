2590dc900eade23ba9cfd53df9aff38269b65cf6
git-infinianti <infante.joshua1994@gmail.com>
2023-09-27 22:50:57 -0700

    fixing

--- pages/2_IPFS.py
+++ pages/2_IPFS.py
@@ -20,13 +20,12 @@ import streamlit_tags as stt
 from httpx import post, Headers
 from ipfs_api import publish
 from json import dumps
-Headers()
 
 if load_dotenv(find_dotenv()): api_key = getenv('DBTOKEN')
 else: api_key = st.secrets['DBTOKEN']
 st.write(api_key)
 LOGGER = get_logger(__name__)
-auth = {'Authorization': f'Bearer {api_key}'}
+auth = {'Authorization': f'Bearer {b64encode(bytes(api_key, "utf-8"))}'}
 endpoint = r'https://api.web3.storage'
 
 
