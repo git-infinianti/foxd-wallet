855a9c6d7520cbcad0b44953067495cdf11d8d76
git-infinianti <infante.joshua1994@gmail.com>
2023-09-28 04:35:16 -0700

    try put

--- pages/2_IPFS.py
+++ pages/2_IPFS.py
@@ -65,7 +65,7 @@ def upload_nft():
                 st.write(st.session_state['cid'])
             except:
                 try:
-                    ret = client.post(url=endpoint + '/upload', content=loaded_file)
+                    ret = client.put(url=endpoint + '/upload', content=loaded_file)
                     cid = loads(ret.content)
                     st.session_state['cid'] = cid
                     st.write(cid)
